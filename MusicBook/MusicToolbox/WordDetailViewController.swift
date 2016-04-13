dd17db578a2bee339da112c6f6a743b388beae24
An Wu <awu@comprehend.com>
2016-04-13 10:04:18 -0700

    Fix scrolling and trim list items

--- MusicBook/MusicToolbox/WordDetailViewController.swift
+++ MusicBook/MusicToolbox/WordDetailViewController.swift
@@ -19,17 +19,35 @@ class WordDetailViewController: UIViewController {
         
         self.navigationItem.title = word.spelling!
 
-        explanationBox.textContainerInset = UIEdgeInsetsMake(20, 30, 0, 30)
+        explanationBox.textContainerInset = UIEdgeInsetsMake(15, 30, 0, 30)
         
         let mutableAttributedString = NSMutableAttributedString()
         word.explanations!.enumerate().forEach({ (index, explanationObj) in
             let explanation = explanationObj as! WordExplanation
-            let attributedString = try? NSAttributedString(data: ("\(index + 1). " + explanation.encodedStr! + "<br /><br />" + "<style>* {font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 16px; line-height: 150%;}</style>").dataUsingEncoding(NSUTF8StringEncoding)!, options:[NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding], documentAttributes: nil)
+            
+            let attributedString = try? NSAttributedString(data: ("\(index + 1). " + cleanEncodedStr(explanation.encodedStr!) + "<br /><br />" + "<style>* {font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 16px; line-height: 150%;}</style>").dataUsingEncoding(NSUTF8StringEncoding)!, options:[NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding], documentAttributes: nil)
             mutableAttributedString.appendAttributedString(attributedString!)
         })
 
         explanationBox.attributedText = mutableAttributedString
         explanationBox.showsVerticalScrollIndicator = false
+        explanationBox.scrollEnabled = false
+    }
+    
+    func cleanEncodedStr(encodedStr: String) -> String {
+        
+        return encodedStr.componentsSeparatedByString("<li>").count == 2 ?
+            encodedStr
+                .stringByReplacingOccurrencesOfString("<li>", withString: "")
+                .stringByReplacingOccurrencesOfString("</li>", withString: "")
+                .stringByReplacingOccurrencesOfString("<ul>", withString: "")
+                .stringByReplacingOccurrencesOfString("</ul>", withString: "")
+            : encodedStr
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        explanationBox.scrollEnabled = true
     }
     
     override func didReceiveMemoryWarning() {
